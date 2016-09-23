338bd6fde583aad18b63b0b8bd2c68579d4c3b11
rwg9692 <rwg9692@rit.edu>
2016-09-23 19:56:03 -0400

    Added menu and GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Initialization, executes before Start()
+	void Awake () {
+        //If there's already a GM in the scene, destroy this one so no duplicates
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);//Don't destroy this object, it'll persist between scenes
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void loadScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+}
