1ff4fc7e4a387bed542323b2a40c4fd95101b792
bowdenblair <bmb1829@g.rit.edu>
2016-09-28 16:40:51 -0400

    Control changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,19 +3,27 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public GameObject player;
-    public Rigidbody2D playerRB;
+    private Rigidbody2D playerRB;
+    private Vector2 movementVector;
+    public float movementSpeed = 200.0f;
 
 	// Use this for initialization
 	void Start () {
-        playerRB = player.GetComponent<Rigidbody2D>();
-	}
+        playerRB = GetComponent<Rigidbody2D>();
+        Debug.Log(playerRB);
+        movementVector = new Vector2(0.0f, 0.0f);	}
 	
 	// Update is called once per frame
 	void Update () {
-	    
         //player controls go here
-        
+        movementVector.x = Input.GetAxis("Left Stick X Axis P1") * movementSpeed;
+        Debug.Log(Input.GetAxis("Left Stick X Axis P1"));
+        if(Input.GetButton("A"))
+        {
+            movementVector.y = 100.0f;
+        }
 
-	}
+        playerRB.AddForce(movementVector);
+        movementVector = Vector2.zero;
+    }
 }
