4bbc9a11921b5918a5070d4d086c215c6b4cfafc
bowdenblair <bmb1829@g.rit.edu>
2016-09-28 15:39:59 -0400

    Player changes
    woohoo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject player;
+    public Rigidbody2D playerRB;
+
+	// Use this for initialization
+	void Start () {
+        playerRB = player.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        //player controls go here
+        
+
+	}
+}
