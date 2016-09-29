bbb65b2d87b6b33491a7e97f7e280d76df73efe9
rwg9692 <rwg9692@rit.edu>
2016-09-29 18:20:19 -0400

    Improved jumping even more but like, for real

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,14 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    public int playerNum;
+
     private Rigidbody2D playerRB;
     private Vector3 velocity;
     public float movementSpeed = 4f;
-    public float jumpStrength = 8f;
-
-    public float gravity = 1f;
+    public float jumpStrength = 1f;
 
     private bool grounded = true;
-    //private bool pressedLastFrame
 
     public float jumpDuration = 1f;
     private float jumpTimer;
@@ -28,39 +27,36 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate () {
 
         //Get input from left stick and put in x axis of rigidbody's velocity
-        playerRB.velocity = new Vector2(Input.GetAxis("Left Stick X Axis P1") * movementSpeed,playerRB.velocity.y);
+        playerRB.velocity = new Vector2(Input.GetAxis("Left Stick X Axis P" + playerNum) * movementSpeed,playerRB.velocity.y);
 
 
         //If A button initially pressed, start jump w/ burst of upward force - jumpReleased prevents from constantly jumping if you hold it down
-        if (Input.GetButtonDown("A") && grounded)
+        if (Input.GetButtonDown("A P" + playerNum) && grounded)
         {
-            playerRB.AddForce(new Vector2(0, jumpStrength*10));
+            playerRB.velocity = new Vector2(playerRB.velocity.x,0);
+            playerRB.AddForce(new Vector2(0, jumpStrength), ForceMode2D.Impulse);
             grounded = false;
             jumpTimer = 0;
         }
 
         //If A button is held, continue adding force for a bit so you can increase jump height by holding
-        else if (Input.GetButton("A") && jumpTimer < jumpDuration && !grounded)
+        else if (Input.GetButton("A P" + playerNum) && jumpTimer < jumpDuration && !grounded)
         {       
-            playerRB.AddForce(new Vector2(0,jumpStrength));
+            playerRB.AddForce(new Vector2(0,jumpStrength*3));
             jumpTimer += Time.deltaTime;
         }
 
         //If jump button has been released, set jumpReleased accordingly
-        else if (Input.GetButtonUp("A"))
-        { 
+        if (Input.GetButtonUp("A P" + playerNum))
+        {
             jumpTimer = jumpDuration;
         }
     }
 
 
-    void OnCollisionEnter2D(Collision2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Floor")
-        {
-            grounded = true;
-            jumpTimer = 0;
-        }
+        grounded = true;
     }
 
 
