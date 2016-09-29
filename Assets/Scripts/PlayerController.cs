bbc368ecff3403dac90e32b5b88d8cef96d0732a
rwg9692 <rwg9692@rit.edu>
2016-09-28 23:01:01 -0400

    Updated Jumping/Movement
    To do: Attacking!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,69 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D playerRB;
-    private Vector2 movementVector;
-    public float movementSpeed = 200.0f;
+    private Vector3 velocity;
+    public float movementSpeed = 4f;
+    public float jumpStrength = 8f;
+
+    public float gravity = 1f;
+
+    private bool grounded = true;
+    private Transform groundCheck;
+
+    public float jumpDuration = 1f;
+    private float jumpTimer;
+    
 
 	// Use this for initialization
 	void Start () {
         playerRB = GetComponent<Rigidbody2D>();
-        Debug.Log(playerRB);
-        movementVector = new Vector2(0.0f, 0.0f);	}
+        velocity = Vector3.zero;
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+
+        playerRB.velocity = new Vector2(Input.GetAxis("Left Stick X Axis P1") * movementSpeed,playerRB.velocity.y);
         //player controls go here
-        movementVector.x = Input.GetAxis("Left Stick X Axis P1") * movementSpeed;
-        Debug.Log(Input.GetAxis("Left Stick X Axis P1"));
-        if(Input.GetButton("A"))
+        //velocity.x = Input.GetAxis("Left Stick X Axis P1") * movementSpeed;
+
+        if (Input.GetButton("A"))
         {
-            movementVector.y = 100.0f;
+            Vector2 jumpForce = Vector2.zero;
+
+            if (grounded)
+            {
+                jumpForce.y = jumpStrength*5;
+                grounded = false;
+
+                jumpTimer = 0;
+            }
+            else if(jumpTimer < jumpDuration)
+            {
+                jumpTimer += Time.deltaTime;
+                jumpForce.y = jumpStrength;
+            }
+
+            playerRB.AddForce(jumpForce);
+        }
+        else
+        {
+            jumpTimer = jumpDuration;
         }
 
-        playerRB.AddForce(movementVector);
-        movementVector = Vector2.zero;
+        //playerRB.velocity = velocity * Time.deltaTime;
+        //transform.position += (velocity * Time.deltaTime);
     }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Floor")
+        {
+            grounded = true;
+            jumpTimer = 0;
+        }
+    }
+
+
 }
