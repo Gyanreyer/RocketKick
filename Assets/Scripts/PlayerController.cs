3d9ba68a0178d64e9ce0455e8094a980fcf9d7a7
rwg9692 <rwg9692@rit.edu>
2016-09-29 22:41:42 -0400

    Crushed that bug like it was crushing my soul
    But really, don't mess with the rigidbody's velocity because apparently
    it breaks everything.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,6 @@ public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D playerRB;
     public float movementSpeed = 4f;
-    public float jumpStrength = 1f;
-
-    private bool grounded = true;
-
-    public float jumpDuration = 1f;
-    private float jumpTimer;
 
     private Vector2 kickForce;
     private bool kick;
@@ -20,8 +14,6 @@ public class PlayerController : MonoBehaviour {
     public float chargeLength;
 
     public float maxKick;
-
-    private Vector2 velocity;
 
 	// Use this for initialization
 	void Start () {
@@ -31,31 +23,8 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        //Get input from left stick and put in x axis of rigidbody's velocity
-        velocity = new Vector2(Input.GetAxis("Left Stick X Axis P" + playerNum) * movementSpeed,playerRB.velocity.y);
-
-        /*
-        //If A button initially pressed, start jump w/ burst of upward force - jumpReleased prevents from constantly jumping if you hold it down
-        if (Input.GetButton("A P" + playerNum) && grounded)
-        {
-            velocity = new Vector2(playerRB.velocity.x,0);
-            playerRB.AddForce(new Vector2(0, jumpStrength), ForceMode2D.Impulse);
-            grounded = false;
-            jumpTimer = 0;
-        }
-
-        //If A button is held, continue adding force for a bit so you can increase jump height by holding
-        else if (Input.GetButton("A P" + playerNum) && jumpTimer < jumpDuration && !grounded)
-        {       
-            playerRB.AddForce(new Vector2(0,jumpStrength*3));
-            jumpTimer += Time.deltaTime;
-        }
-
-        //If jump button has been released, set jumpReleased accordingly
-        if (Input.GetButtonUp("A P" + playerNum))
-        {
-            jumpTimer = jumpDuration;
-        }*/
+        //Get input from left stick and move position's x axis accordingly
+        transform.position += new Vector3(Input.GetAxis("Left Stick X Axis P" + playerNum) * movementSpeed*Time.deltaTime,0,0);
 
         Vector2 rightStick = new Vector2(Input.GetAxis("Right Stick X Axis P" + playerNum), Input.GetAxis("Right Stick Y Axis P" + playerNum));
 
@@ -71,33 +40,26 @@ public class PlayerController : MonoBehaviour {
         }
         else if(kickForce.sqrMagnitude > 0)
         {
-            kickForce *= (chargeKickTimer / chargeLength) * maxKick;
-            chargeKickTimer = 0;
-
-            
+            kickForce *= (maxKick/2) + (chargeKickTimer / chargeLength) * maxKick;
+            chargeKickTimer = 0;     
 
             kick = true;
         }
     }
 
+    //Fixed update for physics stuff
     void FixedUpdate()
     {
-        playerRB.velocity = velocity;
-
         if (kick)
         {
-            playerRB.AddForce(new Vector2(kickForce.x*10,kickForce.y),ForceMode2D.Impulse);
+            
+            playerRB.AddForce(new Vector2(kickForce.x, kickForce.y),ForceMode2D.Impulse);
             kick = false;
 
             kickForce = Vector2.zero;
         }
 
-    }
-
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        grounded = true;
+        //playerRB.velocity += velocity; THIS CODE BROKE EVERYTHING
     }
 
 
