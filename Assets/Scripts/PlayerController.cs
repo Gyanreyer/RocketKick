fc56fb5eff810a10738ea40167f5630f6afe20f2
rwg9692 <rwg9692@rit.edu>
2016-10-02 02:51:20 -0400

    Oct 1st meeting plus some extra work I did
    Implemented a new system for how points are scored, basically goes in
    rounds where you play until last man standing, they score a point, and
    then it respawns everyone.  Just need to add a little bit to display
    that info in the UI!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,8 @@ public class PlayerController : MonoBehaviour {
     public float movementSpeed = 4f;
 
     private Vector2 kickForce;
-    private bool kick;
+    private bool timeToKick;
+    private bool kicking;
     private float chargeKickTimer;
     public float chargeLength;
 
@@ -19,18 +20,27 @@ public class PlayerController : MonoBehaviour {
     public int maxNumKicks = 3;
     private int kicksLeft;
 
+    private float kickDuration;
+
+    public float maxForce;
+
+    public int livesLeft = 5;
+
     private GameObject feet;
-    private ParticleSystem partSys;
+    private ParticleSystem chargePartSys;
+    private ParticleSystem trailPartSys;
+
+    private GameManager gm;
 
 	// Use this for initialization
 	void Start () {
         playerRB = GetComponent<Rigidbody2D>();
-        partSys = GetComponentInChildren<ParticleSystem>();
+        chargePartSys = transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
+        trailPartSys = GetComponent<ParticleSystem>();
 
-        kicksLeft = maxNumKicks; 
+        kicksLeft = maxNumKicks;
 
-        if(playerNum!=0)
-            feet = GameObject.Find("Feet" + playerNum);
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     public void setPlayerNum(int i)
@@ -43,7 +53,8 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 
         //Get input from left stick and move position's x axis accordingly
-        transform.position += new Vector3(Input.GetAxis("Left Stick X Axis P" + playerNum) * movementSpeed*Time.deltaTime,0,0);
+       // transform.position += new Vector3(Input.GetAxis("Left Stick X Axis P" + playerNum) * movementSpeed*Time.deltaTime,0,0);
+        
 
         Vector2 rightStick = new Vector2(Input.GetAxis("Right Stick X Axis P" + playerNum), Input.GetAxis("Right Stick Y Axis P" + playerNum));
 
@@ -57,17 +68,20 @@ public class PlayerController : MonoBehaviour {
             }
 
         }
-        else if(kickForce.sqrMagnitude > 0 && !kick)
+        else if(kickForce.sqrMagnitude > 0 && !timeToKick)
         {
             float kickMagnitude = (chargeKickTimer / chargeLength) * maxKick;
 
             if (kickMagnitude < minKick)
                 kickMagnitude = minKick;
 
+            kickDuration = chargeKickTimer;
+
             kickForce *= kickMagnitude;
             chargeKickTimer = 0;
 
-            kick = true;
+            timeToKick = true;
+            kicking = true;
 
             kicksLeft--;
             
@@ -84,25 +98,32 @@ public class PlayerController : MonoBehaviour {
             feet.transform.position = transform.position;
 
             BoxCollider2D footCollider = feet.GetComponent<BoxCollider2D>();
-            if (playerRB.velocity.sqrMagnitude > 0)
+            if (kickDuration > 0 && playerRB.velocity.sqrMagnitude > 0)
             {
+                trailPartSys.enableEmission = true;
+
+
+                kickDuration -= Time.deltaTime;
                 footCollider.enabled = true;
                 footCollider.offset = playerRB.velocity.normalized * .5f;
             }
             else
             {
+                trailPartSys.enableEmission = false;
+
                 footCollider.enabled = false;
+                kicking = false;
             }
         }
 
         //This is apparently deprecated now but how the hell else do you set emission rate when emission.rate is read-only       
-        partSys.emissionRate = (40*chargeKickTimer / chargeLength);
+        chargePartSys.emissionRate = (40*chargeKickTimer / chargeLength);
 
         if (chargeKickTimer >= chargeLength)
-            partSys.startColor = new Color(1, 0.5f, 0.25f, 1);
+            chargePartSys.startColor = new Color(1, 0.5f, 0.25f, 1);
 
         else
-            partSys.startColor = Color.yellow;
+            chargePartSys.startColor = Color.yellow;
 
 
     }
@@ -110,22 +131,42 @@ public class PlayerController : MonoBehaviour {
     //Fixed update for physics stuff
     void FixedUpdate()
     {
-        if (kick && kicksLeft > 0)
+
+        playerRB.AddForce(new Vector2(Input.GetAxis("Left Stick X Axis P" + playerNum) * (Mathf.Abs(playerRB.velocity.magnitude) > 1? movementSpeed:movementSpeed*10), 0));
+
+        if (timeToKick && kicksLeft > 0)
         {
             playerRB.velocity = Vector2.zero;
 
             playerRB.AddForce(Vector2.ClampMagnitude(kickForce, maxKick),ForceMode2D.Impulse);
-            kick = false;
+            timeToKick = false;
 
             kickForce = Vector2.zero;
         }
+
+        playerRB.velocity = Vector2.ClampMagnitude(playerRB.velocity,maxForce);
+
     }
+
+    void Die()
+    {
+        transform.position = new Vector3(0,5,0);
+        playerRB.velocity = Vector2.zero;
+
+        kicksLeft = maxNumKicks;
+        kicking = false;
+
+        livesLeft--;
+
+    }
+
 
     void OnCollisionStay2D(Collision2D other)
     {
         if(other.gameObject.tag == "Floor")
         {
             kicksLeft = maxNumKicks;
+            kicking = false;
         }
     }
 
@@ -134,7 +175,7 @@ public class PlayerController : MonoBehaviour {
         //If you collide with feet and they're not your own, you died
         if(other.gameObject.tag == "Feet" && other.gameObject.name != feet.name)
         {
-            Destroy(transform.parent.gameObject);//Destroy parent gameobject to get rid of the whole thing - we need to handle this some other way
+            gm.killPlayer(playerNum-1);
         }
     }
 
